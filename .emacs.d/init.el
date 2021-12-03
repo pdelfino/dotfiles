e5f89ab96aed8bc89f8af827d10a2c076b3854c0
Pedro Delfino <p.delfino01@gmail.com>
2021-12-03 19:28:17 -0300

    First commit

---
+++ .emacs.d/init.el
@@ -0,0 +1,28 @@
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(slime-setup '(slime-fancy slime-asdf slime-indentation slime-sbcl-exts slime-scratch))
+
+(setq slime-lisp-implementations
+      '((sbcl ("/home/pedro/projects/nyxt.sh" ""))))
+
+(setq inhibit-startup-screen t)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(slime use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )

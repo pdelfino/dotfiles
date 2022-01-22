9524f2d42e1d2e0637cc139d78ec02b79b1c913c
Pedro Delfino <p.delfino01@gmail.com>
2022-01-22 08:28:44 -0300

    stuff from elpa that I do not understand, haha

---
+++ .emacs.d/elpa/ivy-prescient-20210425.1720/ivy-prescient-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from ivy-prescient.el  -*- no-byte-compile: t -*-
+(define-package "ivy-prescient" "20210425.1720" "prescient.el + Ivy" '((emacs "25.1") (prescient "5.1") (ivy "0.11.0")) :commit "292ac9fe351d469f44765d487f6b9a1c1a68ad1e" :authors '(("Radon Rosborough" . "radon.neon@gmail.com")) :maintainer '("Radon Rosborough" . "radon.neon@gmail.com") :keywords '("extensions") :url "https://github.com/raxod502/prescient.el")

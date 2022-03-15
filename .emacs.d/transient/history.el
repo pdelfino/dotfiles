6ae103483713600497168bb668445b043f089cb6
Pedro Delfino <p.delfino01@gmail.com>
2022-03-15 12:26:22 -0300

    everything saved!

--- .emacs.d/transient/history.el
+++ .emacs.d/transient/history.el
@@ -3,19 +3,28 @@
  (magit-blame
   ("-w"))
  (magit-branch nil)
- (magit-commit nil)
+ (magit-commit nil
+               ("--reuse-message=ORIG_HEAD"))
  (magit-diff
   ("--no-ext-diff" "--stat"))
  (magit-dispatch nil)
  (magit-fetch nil)
  (magit-log
-  ("-n256" "--graph" "--decorate"))
- (magit-pull nil)
+  ("-n256" "--graph" "--decorate")
+  ("-n256" "--author=Pedro Delfino" "--graph" "--decorate")
+  ("-n256" "--author=Pedro Delfino" "--grep= 79504ca" "--graph" "--decorate"))
+ (magit-log:--grep " 79504ca")
+ (magit-pull nil
+             ("--rebase"))
  (magit-push nil
              ("--force-with-lease")
              ("--force"))
  (magit-rebase
   ("--autostash")
   nil)
+ (magit-reset nil)
+ (magit-revision-history "ORIG_HEAD")
  (magit-stash nil)
- (magit-tag nil))
+ (magit-submodule nil)
+ (magit-tag nil)
+ (magit:--author "Pedro Delfino"))

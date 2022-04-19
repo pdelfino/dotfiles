62d2c1c7cc078803c1bbead6dd8fc8ed5bbe5785
Pedro Delfino <p.delfino01@gmail.com>
2022-04-19 17:50:35 -0300

    add hydra... not sure it is going to be good... John uses it, David Wilson also recommends it...

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -174,6 +174,15 @@
 (general-define-key
   "C-x C-b" 'counsel-switch-buffer)
 
+;; Insert hydra, not sure if it is going to be helpful...
+(use-package hydra)
+
+(defhydra hydra-text-scale (:timeout 4)
+  "scale text"
+  ("j" text-scale-increase "in")
+  ("k" text-scale-decrease "out")
+  ("f" nil "finished" :exit t))
+
 ;; Magit configuration
 (use-package magit
   :custom

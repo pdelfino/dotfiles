7c2b5cd63723e35e5f472e021226d6955be1ef8d
Pedro Delfino <p.delfino01@gmail.com>
2022-04-28 12:58:24 -0300

    colocar tamanhos diferentes dependendo da hierarquia dos headings

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -236,6 +236,18 @@
   :custom
   (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
 
+;; change the size of font size
+;; Set faces for heading levels
+(dolist (face '((org-level-1 . 1.2)
+                (org-level-2 . 1.1)
+                (org-level-3 . 1.05)
+                (org-level-4 . 1.0)
+                (org-level-5 . 1.1)
+                (org-level-6 . 1.1)
+                (org-level-7 . 1.1)
+                (org-level-8 . 1.1)))
+  (set-face-attribute (car face) nil :font "Cantarell" :weight 'regular :height (cdr face)))
+
 ;; Inserting wakatime in emacs
 (global-wakatime-mode)
 (setq wakatime-api-key "37bc2977-bd5e-4794-983d-c88624ec6b32")

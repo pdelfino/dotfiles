3288af467eebc258bcc0dbd345a2d28c953f6d6c
Pedro Delfino <p.delfino01@gmail.com>
2022-04-28 12:58:07 -0300

    colocar bolinhas para ter ao invés de asteriscos ****

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -229,6 +229,12 @@
   :config
   (setq org-ellipsis " ▾"));; instead of having `...' there is `▾'.
 
+;; customize *** in org-mode
+(use-package org-bullets
+  :after org
+  :hook (org-mode . org-bullets-mode)
+  :custom
+  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
 
 ;; Inserting wakatime in emacs
 (global-wakatime-mode)

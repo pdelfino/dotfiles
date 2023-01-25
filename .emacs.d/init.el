362b0eb9a6d33e6ec62136a7f17e4276bec62105
Pedro Delfino <p.delfino01@gmail.com>
2023-01-25 13:25:42 -0300

    macOS: add transpose-frame and pdf-tools!
    Now I can read pdfs in Emacs :)

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -745,3 +745,23 @@ the right."
 
 ;; It lets you use a new minibuffer when you're in the minibuffer
 (setq enable-recursive-minibuffers t)
+
+;; Install transpose-frame
+(use-package transpose-frame
+  :ensure t)
+
+;; Use Emacs as pdf viewer. Source:
+;; https://emacs.stackexchange.com/questions/13314/install-pdf-tools-on-emacs-macosx/22591#22591
+;;; Install epdfinfo via 'brew install pdf-tools --HEAD' and then install the
+;;; pdf-tools elisp via the use-package below. To upgrade the epdfinfo
+;;; server, just do 'brew upgrade pdf-tools' prior to upgrading to newest
+;;; pdf-tools package using Emacs package system. If things get messed
+;;; up, just do 'brew uninstall pdf-tools', wipe out the elpa
+;;; pdf-tools package and reinstall both as at the start.
+(use-package pdf-tools
+      :ensure t
+      :config
+      (custom-set-variables
+        '(pdf-tools-handle-upgrades nil)) ; Use brew upgrade pdf-tools instead.
+     (setq pdf-info-epdfinfo-program "/usr/local/bin/epdfinfo"))
+     (pdf-tools-install)
